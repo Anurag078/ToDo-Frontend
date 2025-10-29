1f570c46c2824e69ab7508b9cfb5c4462075f0b2
Anurag <anuragarya168@gmail.com>
2025-10-29 10:16:09 +0530

    Initial frontend upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
+// import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegisterComponent,
+    HomeComponent,
+    LoginComponent,
+    AddTaskComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [
+    // provideClientHydration()
+    provideHttpClient(withFetch()),
+  
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://todoapi-l1xo.onrender.com/api'
+};
